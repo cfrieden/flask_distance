bcf59f95ab4eb0e1a6baca519a6781694ddbad4d
Caden Friedenbach <cfrieden@redhat.com>
2018-04-15 20:26:13 -0400

    simple web app to calculate distance using a web form

---
+++ distance_app.py
@@ -0,0 +1,46 @@
+from flask import Flask, abort, flash, redirect, render_template, request, url_for
+from vincenty import vincenty
+from math import log10
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'ghtyofewnh5678ngh3rt'
+
+@app.route('/')
+def show_form():
+    return render_template('distance.html')
+
+@app.route('/distance', methods=['POST','GET'])
+def distance():
+    enveritas_office=(40.7511532, -73.9786299)
+    #If post from form calculate from form values
+    if request.method == 'POST':
+        point = (float(request.form['longitude']), float(request.form['latitude']))
+        flash('The point longitude : {0} and latitude : {1} is {2} km away'.format( request.form['longitude'], request.form['latitude'], vincenty(enveritas_office,point)))
+    #if get with query parameters
+    elif request.method == 'GET':
+        #make sure query parameters are provided
+        if request.args.get('longitude') == None or request.args.get( 'latitude') == None:
+            flash('Could not calculate please provide both logitude and latitude values')
+            return redirect(url_for('show_form'))
+        #make sure values are valid lat long
+        elif not        _valid_number(request.args.get('longitude'),-180,180,0.0000001) or not _valid_number(request.args.get('latitude'),-90,90,0.0000001):
+            flash('Could not calculate please provide valid logitude and latitude values')
+            return redirect(url_for('show_form'))
+        point = (float(request.args.get('longitude')), float(request.args.get('latitude')))
+        flash('The point longitude : {0} and latitude : {1} is {2} km away'.format( request.args.get('longitude'), request.args.get('latitude'), vincenty(enveritas_office,point)))
+    return redirect(url_for('show_form'))
+
+def _valid_number(input,min,max,step):
+    try:
+        f = float(input)
+        if f < min or f > max:
+            return False
+    except ValueError:
+        print('bad value')
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    app.run()
